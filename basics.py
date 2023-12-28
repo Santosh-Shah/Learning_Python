fae28a80b1c8de8eff4db6a081343dec8e260c86
Santosh Shah <santoshofficial679@gmail.com>
2023-12-28 20:05:11 +0545

    python started

---
+++ basics.py
@@ -0,0 +1,21 @@
+# print("Hariom Shah")
+
+if 5 > 2:
+  print("5 is greater number");
+else :
+  print("2 is greater");
+
+
+
+#single line comments 
+# hello 
+# Hi
+# this is comments
+
+
+# multiple line comments
+"""
+Hello 
+hi
+how are you
+"""
