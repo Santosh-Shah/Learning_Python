473b7e97383916c2612b171bf332497f67af0a59
Santosh Shah <santoshofficial679@gmail.com>
2023-12-29 23:18:20 +0545

    varialbes completed

---
+++ comments.py
@@ -0,0 +1,8 @@
+#single line comments
+# it id demoted by #
+
+
+#multiple line comments is denoted by
+"""
+Hello i m multiple line comments
+"""
---
+++ variables.py
@@ -0,0 +1,18 @@
+# Varialbles
+print("learning variables")
+
+a = 3
+print(a)
+
+a = "Hariom Shah"
+print(a)
+
+name = a
+print(name)
+
+bol = True
+bol1 = False
+print(bol1)
+
+non = None
+print(non)
