aa7c10c5734944784b573571a8d62f1e16ae284c
Santosh Shah <santoshofficial679@gmail.com>
2024-01-03 10:52:57 +0545

    c6_Lists just started

--- c2_comments.py
+++ c2_comments.py
@@ -1,8 +1,8 @@
-#single line comments
+# single line comments
 # it id demoted by #
 
 
-#multiple line comments is denoted by
+# multiple line comments is denoted by
 """
 Hello i m multiple line comments
 """
