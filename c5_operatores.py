9ff2a56dc0579368387b66e50d122cc21ad55fa9
Santosh Shah <santoshofficial679@gmail.com>
2024-01-03 10:40:25 +0545

    c5 operators topic completed

---
+++ c5_operatores.py
@@ -0,0 +1,59 @@
+# Python divides the operators in the following groups:
+#
+# Arithmetic operators
+# x = 12
+# y = 3
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+# print(x % y)
+# print(x ** y)
+# print(x // y)
+
+
+# Assignment operators
+# x = 5
+# y = 2
+# x += 3
+# x *= 2
+# print(x)
+
+
+# Comparison operators
+# x = 5
+# y = 10
+# print(x == y)
+# print(x != y)
+# print(x > y)
+# print(x < y)
+# print(x >= y)
+# print(x <= y)
+
+# Logical operators
+# x = 10
+# y = 15
+# print(x < 5 and y < 100)
+# print(x < 11 or y > 14)
+# print(not (x > 10 and y < 13))
+
+
+# Identity operators
+# x = ["apple", "banana"]
+# y = ["apple", "banana"]
+# z = x
+#
+# print(x is z)
+# print(x is not y)
+
+
+# Membership operators
+# x = ["apple", "Santosh"]
+# print("Santosh" in x)
+# print("apple" not in x)
+
+
+# Bitwise operators
+print(6 ^ 3)
+print(2 & 3)
+print(4 | 5)
