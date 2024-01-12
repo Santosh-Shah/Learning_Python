07e85df939e453b9c9a1b98fc8e55b8644d02c0a
Santosh Shah <santoshofficial679@gmail.com>
2024-01-12 12:04:16 +0545

    adding in list item is completed

---
+++ c6_Lists/addListItems.py
@@ -0,0 +1,13 @@
+list1 = ["Hariom", "Santosh", "Harindra", "cherry", "apple", "banana"]
+# list1.append("Ravi")
+
+list1.insert(4, "Kunal")
+
+list2 = ["Hanuman", "Balbir", 'Ganesh']
+list1.extend(list2)
+
+list3 = ("Krishna", "Gopal")
+list1.extend(list3)
+
+
+print(list1)
