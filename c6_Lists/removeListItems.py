e9fb49a0a8bee2449f5c094b9469bb785e6c6231
Santosh Shah <santoshofficial679@gmail.com>
2024-01-12 12:08:55 +0545

    remove list items completed

---
+++ c6_Lists/removeListItems.py
@@ -0,0 +1,15 @@
+list1 = ["Hariom", "Santosh", "cherry", "Harindra", "cherry", "apple", "banana"]
+# list1.remove("cherry")
+
+# list1.remove("cherry")
+
+# list1.pop(1)
+
+# list1.pop()
+
+# del list1[0]
+
+# del list1
+
+list1.clear()
+print(list1)
