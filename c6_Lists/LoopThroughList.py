379eb097933f750f5ec863164e044cf30ab3d30b
Santosh Shah <santoshofficial679@gmail.com>
2024-01-12 12:16:40 +0545

    Loop throught list completed

---
+++ c6_Lists/LoopThroughList.py
@@ -0,0 +1,16 @@
+list1 = ["Hariom", "Santosh", "Harindra", "cherry", "apple", "banana"]
+
+# for x in list1:
+#     print(x)
+
+# for i in range(len(list1)):
+#     print("print index wise: ", i, "th: ", list1[i])
+#     # print(list1[i])
+
+
+# i = 0
+# while i < len(list1):
+#     print("print index wise: ", i, "th: ", list1[i])
+#     i += 1
+
+[print(x) for x in list1]
