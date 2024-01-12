a2fc3ffb63c7bc309e49c449369f64cf735e3b0f
Santosh Shah <santoshofficial679@gmail.com>
2024-01-12 11:57:33 +0545

    change item in list is completed

--- c6_Lists/changeListItems.py
+++ c6_Lists/changeListItems.py
@@ -1,1 +1,13 @@
 list1 = ["Hariom", "Santosh", "Harindra", "cherry", "apple", "banana"]
+# list1[2] = "Ravi"
+
+# list1[1:3] = ["Rohit", 'Ravi']
+
+# list1[1:2] = ["Rohit", "Ravi"]
+
+# list1[1:3] = ["Rohit"]
+
+list1.insert(2, "Rohit")
+print(list1)
+
+
